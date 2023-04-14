612540e2125dba0612a0f2ac13e0d829ef09eb5a
nrb04 <nredoyan@gmail.com>
2023-04-15 01:14:37 +0600

    login added

---
+++ firebase_with_react/src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5Vc3RZaJmCXK1QRhmfZyPj_Es_fQAlFw",
+  authDomain: "fir-8f6ff.firebaseapp.com",
+  projectId: "fir-8f6ff",
+  storageBucket: "fir-8f6ff.appspot.com",
+  messagingSenderId: "762153791347",
+  appId: "1:762153791347:web:cfd03c9a91eddd31219f06"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
